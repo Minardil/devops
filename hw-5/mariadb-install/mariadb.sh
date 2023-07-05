9287317fdec47e4ea52c86462bcbf62e736e1bdf
konstantin.bogdanov <konstantin.bogdanov@corp.mail.ru>
2023-07-05 23:18:01 +0300

    hw5

---
+++ hw-5/mariadb-install/mariadb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y install mariadb-server
+
+sudo mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('123');FLUSH PRIVILEGES;"
+printf "123\n n\n n\n n\n y\n y\n y\n" | sudo mysql_secure_installation
